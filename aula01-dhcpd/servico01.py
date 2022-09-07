9457ab03e7e95dbc679c725c8bce5bf83d49327a
None <None>
2022-09-07 14:25:06 -0700

    fix bugs and add more functions

--- aula01-dhcpd/servico01.py
+++ aula01-dhcpd/servico01.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 from mininet.net import Mininet
 from mininet.node import Controller, RemoteController, OVSController
@@ -10,11 +10,25 @@ from mininet.log import setLogLevel, info
 from mininet.link import TCLink, Intf
 from subprocess import call
 
+import os
+import time
+
+CLIENTS = 50
+
+def _waitForanIP(host):
+    info('%s waiting for an IP address' % host.name)
+    while True:
+        host.defaultIntf().updateIP()
+        if host.IP():
+            break
+        info('.')
+        time.sleep(0.1)
+    info('\n')
 
 def run_dhcpd_server(host):
     srv = "dhcpd"
     cmd = "/usr/sbin/%s " % (srv)
-    cmd += "-pf %s.pid -cf %s.conf -lf %s.leases" % (srv, srv, srv)
+    cmd += "-pf %s.pid -cf %s.conf -lf %s.leases " % (srv, srv, srv)
     cmd += "> dhcpd.log 2>&1"
     host.cmd("echo > %s.leases" % (srv))
     host.cmd(cmd)
@@ -22,35 +36,52 @@ def run_dhcpd_server(host):
 def run_dhcp_client(host):
     cli = "dhclient"
     name = host.name
-    cmd = "/usr/sbin/%s -v -nw %s-eth0" % (cli, name)
-    host.cmd(cmd)
+    iface = host.defaultIntf()
+    cmd = "/usr/sbin/%s -v -nw %s -pf dhclient-%s.pid" % (cli, iface, name)
+    if (not host.dhcpServer):
+        host.cmd(cmd)
+        _waitForanIP(host)
+
+def clean():
+    info(' *** Cleaning up the previous setup...\n')
+    os.system("killall -9 -w dhcpd")
+    os.system("killall -9 -w dhclient")
+    os.system("rm -f dhcpd.pid")
+    os.system("rm -f dhclient*.pid")
+    os.system("mn -c")
 
 
 def myNetwork():
-    net = Mininet( topo=None,
-                   build=False )
+    net = Mininet()
 
     info( '*** Add switches\n')
     s1 = net.addSwitch('s1', cls=OVSKernelSwitch, failMode='standalone')
 
     info( '*** Add Server\n')
+    # aqui vocês devem inicializar o host "servidor"
 
-    info( '*** Add hosts and links\n')
+    info( '*** Add Clients and links\n')
+    for i in range(1, CLIENTS+1):
+    # aqui vocês devem inicializar os clientes
 
     info( '*** Starting network\n')
     net.build()
 
-    info( '*** Starting Services\n')
-
     info( '*** Starting switches\n')
-    net.get('s1').start([])
+    net.start()
 
-    info( '*** Post configure switches and hosts\n')
+    info( '*** Starting Services\n')
+    # aqui vocês devem inicializar o serviço
+
+    info( '*** Starting Clients\n')
+    for host in net.hosts:
+        # aqui vocês devem inicializar os clientes
 
     CLI(net)
     net.stop()
 
 if __name__ == '__main__':
+    clean()
     setLogLevel( 'info' )
     myNetwork()
-
+    clean()
